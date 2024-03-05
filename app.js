c8033069c80d53505a0ce94e81c609753fabc2c6
bharath9812 <mandadhibharathsimham@gmail.com>
2024-03-05 11:43:33 +0530

    signing cookies

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 const cookieParser = require('cookie-parser');
 
-app.use(cookieParser());
+app.use(cookieParser('skybison')); // skybison is secretkey/lock/encryptor to sign the cookie
 
 app.get('/greet', (req, res) => {
     const { name = 'No=name' } = req.cookies;
@@ -19,6 +19,21 @@ app.get('/setname', (req, res) => {
     res.send('Cookie sent');
 })
 
+// singing cookies : to validate the cookies that they are tampered with info it contains,
+// and confirm legitimacy of the cookie
+// to verify the integrity of that cookie : signed cookies are used
+app.get('/signed', (req, res) => {
+    res.cookie('show', 'avatar', { signed: true });
+    res.send('cookie/movie is signed');
+})
+
+
+//
+
+app.get('/verify', (req, res) => {
+    // res.send(req.cookies); // this will not have singed cookies
+    res.send(req.signedCookies);
+})
 app.listen(3000, () => {
     console.log('workin at 3000 boss')
 })
